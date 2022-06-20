94aee3f4336d97902b2715b9dc082b36e4b66771
huanqi1997 <1136419606@qq.com>
2022-06-20 16:45:49 +0800

    完成了登录和开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,65 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
+$(function(){
+  $("#login-zc").on('click',function(){
+    $('.logindl').show()
+    $('.loginzc').hide()
   })
-
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
+  $("#login-dl").on('click',function(){
+    $('.logindl').hide()
+    $('.loginzc').show()
   })
-
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  // 去网站 查询并使用变量接受
+  var form =layui.form
+  var layer=layui.layer
+  // 从网站接受过来的正则 必须先定义
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
-      }
-    }
-  })
+    pass: [
+      /^[\S]{6,12}$/
+      ,'密码必须6到12位,且不能出现空格'
+    ] ,
+   repass:function(value){
+    var req=$(".loginzc [name=password]").val()
 
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
-  })
+    if(req!==value){return '两次密码不一致'}
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
-    e.preventDefault()
-    $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
-        }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
-      }
-    })
+    
+   }
   })
+  // 给注册页面绑定 submit事件 阻止默认行为 调用psot 接口 填写值 为'#form-zc [name=unname]').val(),password:$('#form-zc [name=password]').val()
+
+  $('#form-zc').on('submit',function(e){
+e.preventDefault()
+var data={username:$('#form-zc [name=unname]').val(),password:$('#form-zc [name=password]').val()}
+$.post('/api/reguser',data,function(res){
+  // 判断状态 
+  if(res.status!==0){
+    // 调用layui弹框
+    return layer.msg(res.message);;
+  }
+ 
+  layer.msg('注册成功')
+  // 模拟点击事件 
+  $('#login-zc').click()
+})
+  })
+  $('#form-dl').submit(function(e){
+  e.preventDefault()
+  $.ajax({
+    // http://api-breakingnews-web.itheima.net
+    url:'/api/login',
+    method:'POST',
+    data:$(this).serialize(),
+success:function(res){
+  if(res.status!==0){
+    // 调用layui弹框
+    return layer.msg('登录失败！');
+  }
+  layer.msg('登录成功')
+  // 将登录成功的token保存到localstorage
+  localStorage.setItem('token',res.token)
+  // 跳转到主页
+  location.href='/index.html'
+}
+  })
+    
+      })
 })
